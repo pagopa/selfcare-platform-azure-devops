0f62c8a34b8fb9caf9033b6186462d75587243be
dariopelliccioli <105275365+dariopelliccioli@users.noreply.github.com>
2022-09-09 16:25:09 +0200

    feat: IId 32 aggiunta service connection di uat (#31)
    * IID-32 add UAT variables
    
    * IID-32 Update FE env variables
    
    * IID32 fix precommit
    
    Co-authored-by: dpellicc <dpellicc@everis.com>

--- azure-devops/idpay-projects/06_pipeline_idpay-timeline.tf
+++ azure-devops/idpay-projects/06_pipeline_idpay-timeline.tf
@@ -47,10 +47,10 @@ locals {
     DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
     DEV_CONTAINER_REGISTRY_NAME         = local.aks_cr_name_dev
     DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
-    # UAT_CONTAINER_REGISTRY_SERVICE_CONN  = azuredevops_serviceendpoint_azurecr.cstar-azurecr-uat.service_endpoint_name
-    # UAT_KUBERNETES_SERVICE_CONN          = azuredevops_serviceendpoint_kubernetes.cstar-aks-uat.service_endpoint_name
-    # UAT_CONTAINER_REGISTRY_NAME          = "cstaruacr.azurecr.io"
-    # UAT_AGENT_POOL                       = "cstar-uat-linux"
+    UAT_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_acr_aks_uat_name
+    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
+    UAT_CONTAINER_REGISTRY_NAME         = local.aks_cr_name_uat
+    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
     # PROD_CONTAINER_REGISTRY_SERVICE_CONN = azuredevops_serviceendpoint_azurecr.cstar-azurecr-prod.service_endpoint_name
     # PROD_KUBERNETES_SERVICE_CONN         = azuredevops_serviceendpoint_kubernetes.cstar-aks-prod.service_endpoint_name
     # PROD_CONTAINER_REGISTRY_NAME         = "cstarpacr.azurecr.io"
--- azure-devops/idpay-projects/99_locals.tf
+++ azure-devops/idpay-projects/99_locals.tf
@@ -18,7 +18,7 @@ locals {
     dev_mixpanel_token     = "TODO" //"6e1290bdda5885981a2f443f37444f0f"
     dev_onetrust_domain_id = "TODO" //"a8f58d7a-7f6a-4fe6-ac02-f95bac3876d4-test"
 
-    uat_azure_subscription = "TODO" #azuredevops_serviceendpoint_azurerm.UAT-SERVICE-CONN.service_endpoint_name
+    uat_azure_subscription = local.service_endpoint_azure_uat_id #azuredevops_serviceendpoint_azurerm.UAT-SERVICE-CONN.service_endpoint_name
     uat_cdn_endpoint       = "cstar-u-weu-idpay-idpaycdn-cdn-endpoint"
     uat_cdn_profile        = "cstar-u-weu-idpay-idpaycdn-cdn-profile"
     uat_resource_group     = "cstar-u-idpay-cdn-rg"
@@ -45,22 +45,25 @@ locals {
     dev_react_app_url_fe_pre_login   = "https://api-io.dev.cstar.pagopa.it/idpay/welfare/token"
     dev_react_app_url_fe_login       = "https://dev.selfcare.pagopa.it/auth"
     dev_react_app_url_fe_assistance  = "https://dev.selfcare.pagopa.it/assistenza"
-    dev_react_app_url_fe_landing     = "https://dev.selfcare.pagopa.it/auth/logout" // TODO when the landing will exists, replace this with the correct URL
+    dev_react_app_url_fe_landing     = "https://dev.selfcare.pagopa.it/auth/logout"
     dev_react_app_url_api_initiative = "https://api-io.dev.cstar.pagopa.it/idpay/initiative"
+    dev_react_app_url_api_groups     = "https://api-io.dev.cstar.pagopa.it/idpay/group"
 
     uat_react_app_url_cdn           = "https://welfare.uat.cstar.pagopa.it/"
     uat_react_app_url_storage       = "https://cstaruweuidpayidpaycdnsa.z6.web.core.windows.net/"
     uat_react_app_url_fe_login      = "https://uat.selfcare.pagopa.it/auth"
     uat_react_app_url_fe_assistance = "https://uat.selfcare.pagopa.it/assistenza"
-    uat_react_app_url_fe_landing    = "https://uat.selfcare.pagopa.it/auth/logout" // TODO when the landing will exists, replace this with the correct URL
-    uat_react_app_url_api_portal    = "TODO"
+    uat_react_app_url_fe_landing    = "https://uat.selfcare.pagopa.it/auth/logout"
+    uat_react_app_url_api_portal    = "https://api-io.uat.cstar.pagopa.it/idpay/initiative"
+    uat_react_app_url_api_groups    = "https://api-io.uat.cstar.pagopa.it/idpay/group"
 
     prod_react_app_url_cdn           = "https://welfare.cstar.pagopa.it/"
     prod_react_app_url_storage       = "https://cstarpweuidpayidpaycdnsa.z6.web.core.windows.net/"
     prod_react_app_url_fe_login      = "https://selfcare.pagopa.it/auth"
     prod_react_app_url_fe_assistance = "https://selfcare.pagopa.it/assistenza"
-    prod_react_app_url_fe_landing    = "https://selfcare.pagopa.it/auth/logout" // TODO when the landing will exists, replace this with the correct URL
+    prod_react_app_url_fe_landing    = "https://selfcare.pagopa.it/auth/logout"
     prod_react_app_url_api_portal    = "TODO"
+
 
   }
 
