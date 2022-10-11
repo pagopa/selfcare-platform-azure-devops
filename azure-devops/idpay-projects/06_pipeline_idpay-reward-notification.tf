3b22c9d62a8bb203ff4099efa4d57a1e53d67452
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-10-11 13:09:19 +0200

    feat: IREER-161 idpay-reward-notification pipelines (#35)
    * new application pipelines
    
    * [IREER-161] Fix ACR service connection name
    
    Co-authored-by: dariopelliccioli <105275365+dariopelliccioli@users.noreply.github.com>
    Co-authored-by: Giovanni <g.mancini.0@gmail.com>

---
+++ azure-devops/idpay-projects/06_pipeline_idpay-reward-notification.tf
@@ -0,0 +1,120 @@
+variable "idpay-reward-notification" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "idpay-reward-notification"
+      branch_name     = "refs/heads/release-dev"
+      pipelines_path  = ".devops"
+      yml_prefix_name = null
+    }
+    pipeline = {
+      enable_code_review = true
+      enable_deploy      = true
+      path               = "idpay\\idpay-reward-notification"
+    }
+  }
+}
+
+locals {
+  # global vars
+  idpay-reward-notification-variables = {
+    dockerfile = "Dockerfile"
+  }
+  # global secrets
+  idpay-reward-notification-variables_secret = {
+
+  }
+  # code_review vars
+  idpay-reward-notification-variables_code_review = {
+    sonarcloud_service_conn = "SONARCLOUD-SERVICE-CONN"
+    sonarcloud_org          = var.idpay-reward-notification.repository.organization
+    sonarcloud_project_key  = "${var.idpay-reward-notification.repository.organization}_${var.idpay-reward-notification.repository.name}"
+    sonarcloud_project_name = var.idpay-reward-notification.repository.name
+  }
+  # code_review secrets
+  idpay-reward-notification-variables_secret_code_review = {
+
+  }
+  # deploy vars
+  idpay-reward-notification-variables_deploy = {
+    K8S_IMAGE_REPOSITORY_NAME        = replace(var.idpay-reward-notification.repository.name, "-", "")
+    DEPLOY_NAMESPACE                 = local.domain
+    SETTINGS_XML_RW_SECURE_FILE_NAME = "settings-rw.xml"
+    SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
+    HELM_RELEASE_NAME                = var.idpay-reward-notification.repository.name
+
+    DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_dev_name
+    DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
+    DEV_CONTAINER_REGISTRY_NAME         = local.aks_dev_docker_registry_name
+    DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
+    UAT_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_uat_name
+    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
+    UAT_CONTAINER_REGISTRY_NAME         = local.aks_uat_docker_registry_name
+    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
+    # PROD_CONTAINER_REGISTRY_SERVICE_CONN = azuredevops_serviceendpoint_azurecr.cstar-azurecr-prod.service_endpoint_name
+    # PROD_KUBERNETES_SERVICE_CONN         = azuredevops_serviceendpoint_kubernetes.cstar-aks-prod.service_endpoint_name
+    # PROD_CONTAINER_REGISTRY_NAME         = "cstarpacr.azurecr.io"
+    # PROD_AGENT_POOL                      = "cstar-prod-linux"
+  }
+  # deploy secrets
+  idpay-reward-notification-variables_secret_deploy = {
+
+  }
+}
+
+module "idpay-reward-notification_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.2.0"
+  count  = var.idpay-reward-notification.pipeline.enable_code_review == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.idpay-reward-notification.repository
+  github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
+  path                         = var.idpay-reward-notification.pipeline.path
+
+  pull_request_trigger_use_yaml = true
+
+  variables = merge(
+    local.idpay-reward-notification-variables,
+    local.idpay-reward-notification-variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.idpay-reward-notification-variables_secret,
+    local.idpay-reward-notification-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    local.service_endpoint_io_azure_devops_github_ro_id,
+    local.azuredevops_serviceendpoint_sonarcloud_id,
+  ]
+}
+
+module "idpay-reward-notification_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
+  count  = var.idpay-reward-notification.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.idpay-reward-notification.repository
+  github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
+  path                         = var.idpay-reward-notification.pipeline.path
+  ci_trigger_use_yaml          = true
+
+  variables = merge(
+    local.idpay-reward-notification-variables,
+    local.idpay-reward-notification-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.idpay-reward-notification-variables_secret,
+    local.idpay-reward-notification-variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    local.service_endpoint_io_azure_devops_github_pr_id,
+
+    local.service_endpoint_azure_dev_id,
+    local.service_endpoint_azure_devops_docker_dev_id,
+    azuredevops_serviceendpoint_kubernetes.aks_dev.id
+  ]
+
+}
