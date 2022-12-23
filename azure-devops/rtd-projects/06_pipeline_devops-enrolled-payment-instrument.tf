6b9dc326b1c2326e46210d4be15198bbad22d22b
petretiandrea <andrea.petreti@pagopa.it>
2022-12-23 14:37:56 +0100

    feat: Enrolled pim pipelines to DEV and UAT (#50)
    add enrolled pim pipelines to DEV and UAT

---
+++ azure-devops/rtd-projects/06_pipeline_devops-enrolled-payment-instrument.tf
@@ -0,0 +1,132 @@
+variable "rtd-ms-enrolled-payment-instrument" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "rtd-ms-enrolled-payment-instrument"
+      branch_name     = "refs/heads/develop"
+      pipelines_path  = ".devops"
+      yml_prefix_name = "cstar-rtd"
+    }
+    pipeline = {
+      enable_code_review = true
+      enable_deploy      = true
+      path               = "rtd\\rtd-ms-enrolled-payment-instrument"
+    }
+  }
+}
+
+locals {
+  # global vars
+  rtd-ms-enrolled-payment-instrument-variables = {
+    dockerfile = "Dockerfile"
+  }
+  # global secrets
+  rtd-ms-enrolled-payment-instrument-variables_secret = {
+
+  }
+  # code_review vars
+  rtd-ms-enrolled-payment-instrument-variables_code_review = {
+    sonarcloud_service_conn = "SONARCLOUD-SERVICE-CONN"
+    sonarcloud_org          = var.rtd-ms-enrolled-payment-instrument.repository.organization
+    sonarcloud_project_key  = "${var.rtd-ms-enrolled-payment-instrument.repository.organization}_${var.rtd-ms-enrolled-payment-instrument.repository.name}"
+    sonarcloud_project_name = var.rtd-ms-enrolled-payment-instrument.repository.name
+  }
+  # code_review secrets
+  rtd-ms-enrolled-payment-instrument-variables_secret_code_review = {
+
+  }
+  # deploy vars
+  rtd-ms-enrolled-payment-instrument-variables_deploy = {
+    K8S_IMAGE_REPOSITORY_NAME        = replace(var.rtd-ms-enrolled-payment-instrument.repository.name, "-", "")
+    DEPLOY_NAMESPACE                 = local.domain
+    SETTINGS_XML_RW_SECURE_FILE_NAME = "settings-rw.xml"
+    SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
+    HELM_RELEASE_NAME                = var.rtd-ms-enrolled-payment-instrument.repository.name
+
+    DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_dev_name
+    DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
+    DEV_CONTAINER_REGISTRY_NAME         = local.aks_dev_docker_registry_name
+    DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
+
+    UAT_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_uat_name
+    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
+    UAT_CONTAINER_REGISTRY_NAME         = local.aks_uat_docker_registry_name
+    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
+
+    #    PROD_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_prod_name
+    #    PROD_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_prod
+    #    PROD_CONTAINER_REGISTRY_NAME         = local.aks_prod_docker_registry_name
+    #    PROD_AGENT_POOL                      = local.azdo_agent_pool_prod
+
+  }
+  # deploy secrets
+  rtd-ms-enrolled-payment-instrument-variables_secret_deploy = {
+
+  }
+}
+
+module "rtd-ms-enrolled-payment-instrument_code_review" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.2.0"
+  count  = var.rtd-ms-enrolled-payment-instrument.pipeline.enable_code_review == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.rtd-ms-enrolled-payment-instrument.repository
+  github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
+  path                         = var.rtd-ms-enrolled-payment-instrument.pipeline.path
+
+  pull_request_trigger_use_yaml = true
+
+  variables = merge(
+    local.rtd-ms-enrolled-payment-instrument-variables,
+    local.rtd-ms-enrolled-payment-instrument-variables_code_review,
+  )
+
+  variables_secret = merge(
+    local.rtd-ms-enrolled-payment-instrument-variables_secret,
+    local.rtd-ms-enrolled-payment-instrument-variables_secret_code_review,
+  )
+
+  service_connection_ids_authorization = [
+    local.service_endpoint_io_azure_devops_github_ro_id,
+    local.azuredevops_serviceendpoint_sonarcloud_id,
+  ]
+}
+
+module "rtd-ms-enrolled-payment-instrument_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
+  count  = var.rtd-ms-enrolled-payment-instrument.pipeline.enable_deploy == true ? 1 : 0
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.rtd-ms-enrolled-payment-instrument.repository
+  github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
+  path                         = var.rtd-ms-enrolled-payment-instrument.pipeline.path
+
+  ci_trigger_use_yaml = true
+
+  variables = merge(
+    local.rtd-ms-enrolled-payment-instrument-variables,
+    local.rtd-ms-enrolled-payment-instrument-variables_deploy,
+  )
+
+  variables_secret = merge(
+    local.rtd-ms-enrolled-payment-instrument-variables_secret,
+    local.rtd-ms-enrolled-payment-instrument-variables_secret_deploy,
+  )
+
+  service_connection_ids_authorization = [
+    local.service_endpoint_io_azure_devops_github_pr_id,
+
+    local.service_endpoint_azure_dev_id,
+    local.service_endpoint_azure_devops_docker_dev_id,
+    azuredevops_serviceendpoint_kubernetes.aks_dev.id,
+
+
+    local.service_endpoint_azure_uat_id,
+    local.service_endpoint_azure_devops_docker_uat_id,
+    azuredevops_serviceendpoint_kubernetes.aks_uat.id,
+
+    #    local.service_endpoint_azure_prod_id,
+    #    local.service_endpoint_azure_devops_docker_prod_id,
+    #    azuredevops_serviceendpoint_kubernetes.aks_prod.id
+  ]
+}
