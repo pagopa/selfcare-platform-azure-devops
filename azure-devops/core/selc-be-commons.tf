0bad5ee83763e80f5fc690b30253fa2be59abc01
andrea-putzu <andrea.putzu@pagopa.it>
2023-02-13 16:36:39 +0100

    fix: Update maven_remote_repo_server_id

--- azure-devops/core/selc-be-commons.tf
+++ azure-devops/core/selc-be-commons.tf
@@ -20,7 +20,7 @@ locals {
     settings_xml_rw_secure_file_name = "settings-rw.xml"
     settings_xml_ro_secure_file_name = "settings-ro.xml"
     maven_remote_repo_server_id      = "selc"
-    maven_remote_repo                = "https://pkgs.dev.azure.com/pagopaspa/selfcare-projects/_packaging/selfcare/maven/v1"
+    maven_remote_repo                = "https://pkgs.dev.azure.com/pagopaspa/selfcare-platform-app-projects/_packaging/selfcare-platform/maven/v1"
   }
   # global secrets
   selc-be-commons-variables_secret = {
