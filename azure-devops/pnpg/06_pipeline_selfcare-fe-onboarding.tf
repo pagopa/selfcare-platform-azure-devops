6f78d771c95e7d7358dee3e3ed399a582dfa48d5
andrea-putzu <andrea.putzu@pagopa.it>
2023-03-11 09:44:46 +0100

    fx: update pipelines onboarding pnpg

--- azure-devops/pnpg/06_pipeline_selfcare-fe-onboarding.tf
+++ azure-devops/pnpg/06_pipeline_selfcare-fe-onboarding.tf
@@ -5,7 +5,7 @@ variable "selfcare-onboarding-frontend" {
       name            = "selfcare-onboarding-frontend"
       branch_name     = "refs/heads/main"
       pipelines_path  = ".devops"
-      yml_prefix_name = "pnpg"
+      yml_prefix_name = null
     }
     pipeline = {
       enable_code_review = false
@@ -18,11 +18,7 @@ variable "selfcare-onboarding-frontend" {
 locals {
   # global vars
   selfcare-onboarding-frontend-variables = {
-    settings_xml_rw_secure_file_name = "settings-rw.xml"
-    settings_xml_ro_secure_file_name = "settings-ro.xml"
-    maven_remote_repo_server_id      = "selfcare-platform"
-    maven_remote_repo                = "https://pkgs.dev.azure.com/pagopaspa/selfcare-platform-app-projects/_packaging/selfcare-platform/maven/v1"
-    dockerfile                       = "Dockerfile"
+    default_branch = var.selfcare-onboarding-frontend.repository.branch_name
   }
   # global secrets
   selfcare-onboarding-frontend-variables_secret = {
@@ -30,10 +26,7 @@ locals {
   }
   # code_review vars
   selfcare-onboarding-frontend-variables_code_review = {
-    sonarcloud_service_conn = "SONARCLOUD-SERVICE-CONN"
-    sonarcloud_org          = var.selfcare-onboarding-frontend.repository.organization
-    sonarcloud_project_key  = "${var.selfcare-onboarding-frontend.repository.organization}_${var.selfcare-onboarding-frontend.repository.name}"
-    sonarcloud_project_name = var.selfcare-onboarding-frontend.repository.name
+    danger_github_api_token = "skip"
   }
   # code_review secrets
   selfcare-onboarding-frontend-variables_secret_code_review = {
@@ -41,28 +34,6 @@ locals {
   }
   # deploy vars
   selfcare-onboarding-frontend-variables_deploy = {
-
-    K8S_IMAGE_REPOSITORY_NAME        = replace(var.selfcare-onboarding-frontend.repository.name, "-", "")
-    DEPLOY_NAMESPACE                 = local.domain
-    DEPLOYMENT_NAME                  = "onboarding-frontend"
-    SETTINGS_XML_RW_SECURE_FILE_NAME = "settings-rw.xml"
-    SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
-    HELM_RELEASE_NAME                = var.selfcare-onboarding-frontend.repository.name
-
-    DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_dev_name
-    DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
-    DEV_CONTAINER_REGISTRY_NAME         = local.aks_dev_docker_registry_name
-    DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
-
-    UAT_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_uat_name
-    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
-    UAT_CONTAINER_REGISTRY_NAME         = local.aks_uat_docker_registry_name
-    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
-
-    PROD_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_prod_name
-    PROD_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_prod
-    PROD_CONTAINER_REGISTRY_NAME         = local.aks_prod_docker_registry_name
-    PROD_AGENT_POOL                      = local.azdo_agent_pool_prod
 
   }
   # deploy secrets
@@ -109,6 +80,7 @@ module "selfcare-onboarding-frontend_deploy" {
   ci_trigger_use_yaml = true
 
   variables = merge(
+    local.selc-fe-common-variables_deploy,
     local.selfcare-onboarding-frontend-variables,
     local.selfcare-onboarding-frontend-variables_deploy,
   )
@@ -119,9 +91,12 @@ module "selfcare-onboarding-frontend_deploy" {
   )
 
   service_connection_ids_authorization = [
-    data.azuredevops_serviceendpoint_github.github_ro.service_endpoint_id,
-    data.azuredevops_serviceendpoint_azurerm.azure_dev.service_endpoint_id,
-    local.service_endpoint_azure_devops_docker_dev_id,
-    azuredevops_serviceendpoint_kubernetes.aks_dev.id
+    data.azuredevops_serviceendpoint_github.github_ro.id,
+    data.azuredevops_serviceendpoint_azurerm.azure_dev.id,
+
+    # data.azuredevops_serviceendpoint_github.github_ro.service_endpoint_id,
+    # data.azuredevops_serviceendpoint_azurerm.azure_dev.service_endpoint_id,
+    # local.service_endpoint_azure_devops_docker_dev_id,
+    # azuredevops_serviceendpoint_kubernetes.aks_dev.id
   ]
 }
