d3e461732096a1cab2a5931884e2a7ca7ee4012b
andrea-putzu <andrea.putzu@pagopa.it>
2023-03-13 06:35:30 +0100

    fx: update pipelines onboarding pnpg

--- azure-devops/pnpg/06_pipeline_selfcare-fe-dashboard-admin.tf
+++ azure-devops/pnpg/06_pipeline_selfcare-fe-dashboard-admin.tf
@@ -5,10 +5,10 @@ variable "selfcare-dashboard-admin-microfrontend" {
       name            = "selfcare-dashboard-admin-microfrontend"
       branch_name     = "refs/heads/main"
       pipelines_path  = ".devops"
-      yml_prefix_name = "pnpg"
+      yml_prefix_name = null
     }
     pipeline = {
-      enable_code_review = false
+      enable_code_review = true
       enable_deploy      = true
       path               = "pnpg\\selfcare-dashboard-admin-microfrontend"
     }
@@ -18,11 +18,7 @@ variable "selfcare-dashboard-admin-microfrontend" {
 locals {
   # global vars
   selfcare-dashboard-admin-microfrontend-variables = {
-    settings_xml_rw_secure_file_name = "settings-rw.xml"
-    settings_xml_ro_secure_file_name = "settings-ro.xml"
-    maven_remote_repo_server_id      = "selfcare-platform"
-    maven_remote_repo                = "https://pkgs.dev.azure.com/pagopaspa/selfcare-platform-app-projects/_packaging/selfcare-platform/maven/v1"
-    dockerfile                       = "Dockerfile"
+    default_branch = var.selfcare-pnpg-onboarding-frontend.repository.branch_name
   }
   # global secrets
   selfcare-dashboard-admin-microfrontend-variables_secret = {
@@ -30,10 +26,7 @@ locals {
   }
   # code_review vars
   selfcare-dashboard-admin-microfrontend-variables_code_review = {
-    sonarcloud_service_conn = "SONARCLOUD-SERVICE-CONN"
-    sonarcloud_org          = var.selfcare-dashboard-admin-microfrontend.repository.organization
-    sonarcloud_project_key  = "${var.selfcare-dashboard-admin-microfrontend.repository.organization}_${var.selfcare-dashboard-admin-microfrontend.repository.name}"
-    sonarcloud_project_name = var.selfcare-dashboard-admin-microfrontend.repository.name
+    danger_github_api_token = "skip"
   }
   # code_review secrets
   selfcare-dashboard-admin-microfrontend-variables_secret_code_review = {
@@ -41,28 +34,6 @@ locals {
   }
   # deploy vars
   selfcare-dashboard-admin-microfrontend-variables_deploy = {
-
-    K8S_IMAGE_REPOSITORY_NAME        = replace(var.selfcare-dashboard-admin-microfrontend.repository.name, "-", "")
-    DEPLOY_NAMESPACE                 = local.domain
-    DEPLOYMENT_NAME                  = "dashboard-admin-microfrontend"
-    SETTINGS_XML_RW_SECURE_FILE_NAME = "settings-rw.xml"
-    SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
-    HELM_RELEASE_NAME                = var.selfcare-dashboard-admin-microfrontend.repository.name
-
-    DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_dev_name
-    DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
-    DEV_CONTAINER_REGISTRY_NAME         = local.aks_dev_docker_registry_name
-    DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
-
-    UAT_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_uat_name
-    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
-    UAT_CONTAINER_REGISTRY_NAME         = local.aks_uat_docker_registry_name
-    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
-
-    PROD_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_prod_name
-    PROD_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_prod
-    PROD_CONTAINER_REGISTRY_NAME         = local.aks_prod_docker_registry_name
-    PROD_AGENT_POOL                      = local.azdo_agent_pool_prod
 
   }
   # deploy secrets
@@ -77,7 +48,7 @@ module "selfcare-dashboard-admin-microfrontend_code_review" {
 
   project_id                   = data.azuredevops_project.project.id
   repository                   = var.selfcare-dashboard-admin-microfrontend.repository
-  github_service_connection_id = data.azuredevops_serviceendpoint_github.github_ro.service_endpoint_id
+  github_service_connection_id = data.azuredevops_serviceendpoint_github.github_rw.service_endpoint_id
   path                         = var.selfcare-dashboard-admin-microfrontend.pipeline.path
 
   pull_request_trigger_use_yaml = true
@@ -93,7 +64,7 @@ module "selfcare-dashboard-admin-microfrontend_code_review" {
   )
 
   service_connection_ids_authorization = [
-    data.azuredevops_serviceendpoint_github.github_ro.service_endpoint_id
+    data.azuredevops_serviceendpoint_github.github_pr.service_endpoint_id
   ]
 }
 
@@ -103,12 +74,13 @@ module "selfcare-dashboard-admin-microfrontend_deploy" {
 
   project_id                   = data.azuredevops_project.project.id
   repository                   = var.selfcare-dashboard-admin-microfrontend.repository
-  github_service_connection_id = data.azuredevops_serviceendpoint_github.github_ro.service_endpoint_id
+  github_service_connection_id = data.azuredevops_serviceendpoint_github.github_rw.service_endpoint_id
   path                         = var.selfcare-dashboard-admin-microfrontend.pipeline.path
 
   ci_trigger_use_yaml = true
 
   variables = merge(
+    local.selc-fe-common-variables_deploy-pnpg,
     local.selfcare-dashboard-admin-microfrontend-variables,
     local.selfcare-dashboard-admin-microfrontend-variables_deploy,
   )
@@ -119,9 +91,7 @@ module "selfcare-dashboard-admin-microfrontend_deploy" {
   )
 
   service_connection_ids_authorization = [
-    data.azuredevops_serviceendpoint_github.github_ro.service_endpoint_id,
+    data.azuredevops_serviceendpoint_github.github_pr.service_endpoint_id,
     data.azuredevops_serviceendpoint_azurerm.azure_dev.service_endpoint_id,
-    local.service_endpoint_azure_devops_docker_dev_id,
-    azuredevops_serviceendpoint_kubernetes.aks_dev.id
   ]
 }
