9063f04036ce0f4b95e4421d2f717c5e9332c489
andrea-putzu <andrea.putzu@pagopa.it>
2023-03-25 08:25:26 +0100

    fix: Pnpg External API

--- azure-devops/pnpg/06_pipeline_selfcare-external-api-backend.tf
+++ azure-devops/pnpg/06_pipeline_selfcare-external-api-backend.tf
@@ -42,28 +42,10 @@ locals {
   # deploy vars
   selfcare-external-api-backend-variables_deploy = {
 
-    K8S_IMAGE_REPOSITORY_NAME        = replace(var.selfcare-external-api-backend.repository.name, "-", "")
-    DEPLOY_NAMESPACE                 = local.domain
-    DEPLOYMENT_NAME                  = "external-api-backend"
-    SETTINGS_XML_RW_SECURE_FILE_NAME = "settings-rw.xml"
-    SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
-    HELM_RELEASE_NAME                = var.selfcare-external-api-backend.repository.name
-
-    DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_dev_name
-    DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
-    DEV_CONTAINER_REGISTRY_NAME         = local.aks_dev_docker_registry_name
-    DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
-
-    UAT_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_uat_name
-    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
-    UAT_CONTAINER_REGISTRY_NAME         = local.aks_uat_docker_registry_name
-    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
-
-    PROD_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_prod_name
-    PROD_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_prod
-    PROD_CONTAINER_REGISTRY_NAME         = local.aks_prod_docker_registry_name
-    PROD_AGENT_POOL                      = local.azdo_agent_pool_prod
-
+    k8s_image_repository_name        = replace(var.selfcare-external-api-backend.repository.name, "-", "")
+    deploy_namespace                 = local.domain
+    deployment_name                  = "external-api"
+    helm_release_name                = var.selfcare-external-api-backend.repository.name
   }
   # deploy secrets
   selfcare-external-api-backend-variables_secret_deploy = {
@@ -110,6 +92,7 @@ module "selfcare-external-api-backend_deploy" {
   ci_trigger_use_yaml = true
 
   variables = merge(
+    local.pnpg-be-common-variables_deploy,
     local.selfcare-external-api-backend-variables,
     local.selfcare-external-api-backend-variables_deploy,
   )
