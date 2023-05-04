3a16b06a47062a7d31cfef50d4923a1d7152e014
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-05-04 17:05:54 +0200

    Selc 2100 load tests pipeline (#8)
    * adding run-load-tests pipeline
    
    * pre-commit run
    
    * fixed mandatory parameters
    
    * udate github service
    
    * added service connection towards azure subscription to load tests pipeline
    
    * fix format
    
    ---------
    
    Co-authored-by: andrea-putzu <andrea.putzu@pagopa.it>

---
+++ azure-devops/core/load-tests-run.tf
@@ -0,0 +1,36 @@
+variable "selfcare-load-tests" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "selfcare-load-tests"
+      branch_name    = "main"
+      pipelines_path = "pipelines"
+    }
+  }
+}
+
+module "selfcare-load-tests_run" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_generic?ref=v2.6.5"
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.selfcare-load-tests.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
+  path                         = "${local.selfcare_legacy.pipelines_folder_name}\\${var.selfcare-load-tests.repository.name}"
+  pipeline_name                = "run-load-tests"
+  pipeline_yml_filename        = "run-load-tests.yml"
+  ci_trigger_use_yaml          = true
+
+  variables        = {
+    dev_azure_service_name  = azuredevops_serviceendpoint_azurerm.DEV-SELFCARE.service_endpoint_name
+    uat_azure_service_name  = azuredevops_serviceendpoint_azurerm.UAT-SELFCARE.service_endpoint_name
+    prod_azure_service_name = azuredevops_serviceendpoint_azurerm.PROD-SELFCARE.service_endpoint_name
+  }
+  variables_secret = {}
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id,
+    azuredevops_serviceendpoint_azurerm.DEV-SELFCARE.id,
+    azuredevops_serviceendpoint_azurerm.UAT-SELFCARE.id,
+    azuredevops_serviceendpoint_azurerm.PROD-SELFCARE.id,
+  ]
+}
