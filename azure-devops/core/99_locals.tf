572b05c53cfa0c3f1cc10bf950e5447a95eafbc6
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2023-07-11 17:04:26 +0200

    Feature/selc 2665 (#35)

--- azure-devops/core/99_locals.tf
+++ azure-devops/core/99_locals.tf
@@ -50,6 +50,7 @@ locals {
     dev_onetrust_domain_id = "a8f58d7a-7f6a-4fe6-ac02-f95bac3876d4-test"
     dev_react_app_url_geotaxonomy = "https://api.pdnd.pagopa.it/geo-tax/geotaxonomy"
     dev_aoo_uo_enabled = "true"
+    dev_pt_enabled = "false"
 
     uat_azure_subscription = azuredevops_serviceendpoint_azurerm.UAT-SELFCARE.service_endpoint_name
     uat_cdn_endpoint       = "selc-u-checkout-cdn-endpoint"
@@ -62,7 +63,7 @@ locals {
     uat_onetrust_domain_id = "15a1f042-9257-450f-b9e8-38d019191729"
     uat_react_app_url_geotaxonomy = "https://api.pdnd.pagopa.it/geo-tax/geotaxonomy"
     uat_aoo_uo_enabled = "false"
-
+    uat_pt_enabled = "true"
 
     prod_azure_subscription = azuredevops_serviceendpoint_azurerm.PROD-SELFCARE.service_endpoint_name
     prod_cdn_endpoint       = "selc-p-checkout-cdn-endpoint"
@@ -75,6 +76,7 @@ locals {
     prod_onetrust_domain_id = "084d5de2-d423-458a-9b28-0f8db3e55e71"
     prod_react_app_url_geotaxonomy = "https://api.pdnd.pagopa.it/geo-tax/geotaxonomy"
     prod_aoo_uo_enabled = "false"
+    prod_pt_enabled = "false"
 
     react_app_assistance_email = "areariservata@assistenza.pagopa.it"
 
