b5c89cd41bfe84751cc5fed872d2fd682b9b8f6f
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2023-09-20 17:37:01 +0200

    fix: Add new pipeline for iac-selfcare deploy (#44)

--- azure-devops/iac/00_secrets_kv_selfcare.tf
+++ azure-devops/iac/00_secrets_kv_selfcare.tf
@@ -1,6 +1,66 @@
 #
-# PROD ECOMMERCE KEYVAULT
+# DEV Selfcare Legacy KEYVAULT
 #
+
+module "selfcare_legacy_dev_secrets" {
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v4.13.0"
+
+  resource_group = local.dev_selfcare_key_vault_resource_group
+  key_vault_name = local.dev_selfcare_key_vault_name
+
+  secrets = [
+    "dev-selfcare-aks-apiserver-url",
+    "dev-selfcare-aks-azure-devops-sa-token",
+    "dev-selfcare-aks-azure-devops-sa-cacrt",
+  ]
+}
+
+#
+# UAT Selfcare Legacy KEYVAULT
+#
+
+module "selfcare_legacy_uat_secrets" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v4.13.0"
+
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  resource_group = local.uat_selfcare_key_vault_resource_group
+  key_vault_name = local.uat_selfcare_key_vault_name
+
+  secrets = [
+    "uat-selfcare-aks-apiserver-url",
+    "uat-selfcare-aks-azure-devops-sa-token",
+    "uat-selfcare-aks-azure-devops-sa-cacrt",
+  ]
+}
+
+#
+# PROD Selfcare Legacy KEYVAULT
+#
+
+module "selfcare_legacy_prod_secrets" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v4.13.0"
+
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  resource_group = local.prod_selfcare_key_vault_resource_group
+  key_vault_name = local.prod_selfcare_key_vault_name
+
+  secrets = [
+    "prod-selfcare-aks-apiserver-url",
+    "prod-selfcare-aks-azure-devops-sa-token",
+    "prod-selfcare-aks-azure-devops-sa-cacrt",
+  ]
+}
 
 module "selfcare_dev_secrets" {
 
